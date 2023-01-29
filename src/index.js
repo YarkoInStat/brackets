e3fa1b2ed0c5758b9ddd15ce6c64a1a9aaa708d3
Iaroslav Gritsenko <yaroslav.gritsenko@instatsport.com>
2023-01-30 00:32:32 +0300

    init:brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const stack = [];
+  console.log(str);
+
+  for (let symbol of str) {
+
+    if (symbol === '(' || symbol === '[' || symbol === '{') {
+      stack.push(symbol);
+
+    } else if (symbol === ')' || symbol === ']' || symbol === '}') {
+
+      if (!stack.pop()) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+
+
+
+
+
+
+
 }
