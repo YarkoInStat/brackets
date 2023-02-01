ed1337b95bbe0822ad45692fc8cafb3a39105b3f
Iaroslav Gritsenko <yaroslav.gritsenko@instatsport.com>
2023-02-01 19:12:15 +0300

    init:brackets

--- src/index.js
+++ src/index.js
@@ -1,26 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
 
-  const stack = [];
-  console.log(str);
+//   const stack = [];
+//   const open = bracketsConfig[0];
+//   const close = bracketsConfig[1];
 
-  for (let symbol of str) {
+//   for (let i = 0; i < str.length; i++) {
 
-    if (symbol === '(' || symbol === '[' || symbol === '{') {
-      stack.push(symbol);
+//     if (open === '(' || open === '[' || open === '{') {
+//       stack.push(str[i]);
 
-    } else if (symbol === ')' || symbol === ']' || symbol === '}') {
+//     } else if (close === ')' || close === ']' || close === '}' && !stack.pop()) {
+//       return false;
+    
+//     } else {
+//       stack.push(str[i])
+//     }
 
-      if (!stack.pop()) {
+//   return stack.length === 0;
+
+// }
+
+  let open = [];
+  let close = [];
+  let match;
+  let stack = [];
+
+  bracketsConfig.forEach(brackets => {
+    open.push(brackets[0]);
+    close.push(brackets[1]);
+  })
+
+  for (let i = 0; i < str.length; i++) {
+
+    if ((open.indexOf(str[i]) === -1 || stack.indexOf(str[i]) > -1) && close.indexOf(str[i]) > -1) {
+      match = open[close.indexOf(str[i])];
+      if (stack.length == 0 || (stack.pop() != match)) {
         return false;
       }
+    } else {
+      stack.push(str[i]);
     }
   }
-  return stack.length === 0;
 
-
-
-
-
-
-
-}
+  return (stack.length == 0);
+};
